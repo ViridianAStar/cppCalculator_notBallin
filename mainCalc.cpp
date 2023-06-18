4f2aa375d64196862044040ac9f178492b5dac92
ViridianAStar <88561900+ViridianAStar@users.noreply.github.com>
2023-06-18 16:37:24 +0000

    Started to do the thing.

--- mainCalc.cpp
+++ mainCalc.cpp
@@ -1,1 +1,23 @@
+#include "math.h"
 
+// add 2 numbers 
+double add(double a, double b) {double c = a + b; return c;}
+
+// subtract 2 numbers
+double subtract(double a, double b) {double c = a - b; return c;}
+
+// multiply 2 numbers
+double multiply(double a, double b) {double c = a*b; return c;}
+
+// divide 2 numbers
+double divide(double a, double b) {double c = a/b; return c;}
+
+// modulus of a using b
+int modulo(int a, int b) {int c = a%b; return c;}
+
+// logarithm base b of a
+double logarithm(double a, double b) {double c = logb(a); return c;}
+
+void main(){
+
+}
